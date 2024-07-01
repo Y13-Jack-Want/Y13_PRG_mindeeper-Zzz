fdd1df93c67e2f1664d960e3bf6f7ae7b5496eca
Y13-Jack-Want <st20111@howick.school.nz>
2024-07-02 10:39:09 +1200

    Finished version of basic gui using tkinter

--- Gui_test_1.py
+++ Gui_test_1.py
@@ -10,8 +10,8 @@ from tkinter import *
 
 # initial window varibales
 window_cfg = {
-    "width": 900,
-    "height": 650,
+    "width": 1200,
+    "height": 950,
     "x-shift": 0,
     "y-shift": 0
 }
@@ -33,8 +33,8 @@ difficulties = {
 }
 
 #grid square config
-square_x = 50
-square_y = 50
+square_x = 30
+square_y = 30
 square = {
     "width": square_x, 
     "height": square_y, 
@@ -45,10 +45,15 @@ square = {
 #difficulty select
 difficulty = input("Difficulty: ")
 brd_values = difficulties[difficulty]
+print(brd_values)
 brd_size = brd_values["board"]
+print(brd_size)
 brd_mines = brd_values["mines"]
+print(brd_mines)
 brd_width = brd_size[0]*square["width"]
+print(brd_width)
 brd_height = brd_size[1]*square["height"]
+print(brd_height)
 
 #root window creation
 root = Tk()
@@ -56,7 +61,7 @@ root.title("Bomb wiper")
 root.geometry('{}x{}+{}+{}'.format(window_cfg["width"],window_cfg["height"],window_cfg["x-shift"],window_cfg["y-shift"]))
 
 #mainFrame time :3
-left = Frame(root, bg='#013220', width=600, height=650)
+left = Frame(root, bg='gray', width=600, height=650)
 right = Frame(root, bg='pink', width=300, height=650)
 
 #mainFrame layout
@@ -65,7 +70,9 @@ right.grid(row=0, column=1, sticky="nsew")
 
 #left subframe setup
 header = Frame(left, bg='#E6E6FA', width=600, height=50)
-game = Frame(left, bg='#90EE90', width=brd_width, height=brd_height)
+game = Frame(left, bg='#013220', width=30*square["width"], height=30*square["height"])
+
+#90EE90
 
 #left subframe layout
 header.grid(row=0, column=0, sticky="ew")
